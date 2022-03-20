943c21381b87af6b644c8f81ffda4e2b8ab7a9c6
Atul <atulsarpate96@gmail.com>
2022-03-20 14:23:43 +0530

    first commit

---
+++ django-deployment-master/static/js/myfile.js
@@ -0,0 +1,31 @@
+
+
+
+function showDeveloper_Profile_Geography_All_Countries() {
+console.log("Button clicked");
+var container = document.getElementById("Developer_Profile_Geography")
+// container.innerHTML='<img src="{% static "images/Technology_Most_loved_dreaded_and_wanted_Databases_Loved_vs_Dreaded_pct.png"%}" alt="Uh Oh, didnt show!">'
+// container.innerHTML='<img src="../images/Technology_Most_loved_dreaded_and_wanted_Databases_Loved_vs_Dreaded_pct.png" alt="not found">'
+
+container.innerHTML='<img src="{"images/Technology_Most_loved_dreaded_and_wanted_Databases_Loved_vs_Dreaded_pct.png"%}" alt="Uh Oh, didnt show!">'
+}
+
+
+function showFoo() {
+console.log("Button clicked");
+var container = document.getElementById("hello_div")
+// container.innerHTML='<img src="{% static "images/Technology_Most_loved_dreaded_and_wanted_Databases_Loved_vs_Dreaded_pct.png"%}" alt="Uh Oh, didnt show!">'
+// container.innerHTML='<img src="../images/Technology_Most_loved_dreaded_and_wanted_Databases_Loved_vs_Dreaded_pct.png" alt="not found">'
+
+container.innerHTML='<img src="{"images/Technology_Most_loved_dreaded_and_wanted_Databases_Loved_vs_Dreaded_pct.png"%}" alt="Uh Oh, didnt show!">'
+}
+
+
+function showDeveloper_Profile_Experience_Writing_first_line_of_code_counts() {
+console.log("Button clicked");
+var container = document.getElementById("container_id_Developer_Profile_Experience_Writing_first_line_of_code_counts")
+// container.innerHTML='<img src="{% static "images/Technology_Most_loved_dreaded_and_wanted_Databases_Loved_vs_Dreaded_pct.png"%}" alt="Uh Oh, didnt show!">'
+// container.innerHTML='<img src="../images/Technology_Most_loved_dreaded_and_wanted_Databases_Loved_vs_Dreaded_pct.png" alt="not found">'
+
+container.innerHTML='<img src="{"images/Technology_Most_loved_dreaded_and_wanted_Databases_Loved_vs_Dreaded_pct.png"%}" alt="Uh Oh, didnt show!">'
+}
---
+++ django-deployment-master/templates/first_app/java.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+
+
+function showFoo() {
+console.log("Button clicked");
+var container = document.getElementById("hello_div")
+container.innerHTML='<img src="{% static "images/Technology_Most_loved_dreaded_and_wanted_Databases_Loved_vs_Dreaded_pct.png"%}" alt="Uh Oh, didnt show!">'
+}
+</script>
